492b00e16c84d2b42b66b015e525928521faa69c
MATHEUS FERNANDO FREIRE FERREIRA <matheusfernandofreire@hotmail.com>
2021-05-22 13:08:57 -0300

    Concluído

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'theme';
+export class AppComponent implements OnInit {
+  body = document.body
+
+  ngOnInit() {
+    const theme = localStorage.getItem('theme');
+    const isSolar = localStorage.getItem('isSolar');
+
+    if (theme) {
+      this.body.classList.add(theme);
+      isSolar && this.body.classList.add('solar');
+    }
+  }
+
+  darkTheme(): void {
+    this.body.classList.replace('light', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+
+  ligthTheme(): void {
+    this.body.classList.replace('dark', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+
+  solarTheme(): void {
+    if (this.body.classList.contains('solar')) {
+      this.body.classList.remove('solar');
+      localStorage.setItem('isSolar', JSON.stringify(false));
+    } else {
+      this.body.classList.add('solar');
+      localStorage.setItem('isSolar', JSON.stringify(true));
+    }
+  }
 }
